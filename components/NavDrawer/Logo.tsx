2eeb8bf61b49c4c0abf7528f5d7c6e64e3e6034d
Kyle <noll.kyle@gmail.com>
2024-06-12 11:38:10 -0800

    Make logo redirect to dashboard

--- components/NavDrawer/Logo.tsx
+++ components/NavDrawer/Logo.tsx
@@ -1,8 +1,15 @@
 import { Flex, Image, Text } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
 
 export function Logo() {
+  const router = useRouter()
+
+  function handleClick() {
+    router.push('/dashboard')
+  }
+
   return (
-    <Flex alignItems="center" gap={1}>
+    <Flex alignItems="center" gap={1} onClick={handleClick} cursor="pointer">
       <Image w="48px" h="48px" alt="Ion logo" src="/assets/images/IonLogo.png" />
       <Text fontSize="20px" mt="8px">
         ION PROTOCOL
