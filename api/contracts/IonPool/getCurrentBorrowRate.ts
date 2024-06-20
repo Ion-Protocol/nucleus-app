8ad76b2d02220d3593519df98921a6cdb2cd3d7d
Kyle <noll.kyle@gmail.com>
2024-06-20 09:21:56 -0800

    Add some contract functions

---
+++ api/contracts/IonPool/getCurrentBorrowRate.ts
@@ -0,0 +1,26 @@
+import { ChainKey, chainsConfig } from '@/config/chains'
+import { wagmiConfig } from '@/config/wagmi'
+import IonPool from '@/contracts/IonPool.json'
+import { MarketKey } from '@/types/Market'
+import { Abi } from 'viem'
+import { readContract } from 'wagmi/actions'
+
+export async function getCurrentBorrowRate({
+  ilkIndex,
+  chainKey,
+  marketKey,
+}: {
+  ilkIndex: number
+  chainKey: ChainKey
+  marketKey: MarketKey
+}): Promise<bigint> {
+  const contractAddress = chainsConfig[chainKey].markets[marketKey].contracts.ionPool
+  const totalSupply = (await readContract(wagmiConfig, {
+    abi: IonPool.abi as Abi,
+    address: contractAddress,
+    functionName: 'getCurrentBorrowRate',
+    args: [ilkIndex],
+  })) as bigint
+
+  return totalSupply
+}
