d8b44c26ff4d0cf25595fda889291f5e750e9b3f
Kyle <noll.kyle@gmail.com>
2024-06-24 23:32:53 -0800

    chore: Improve ConnectAwareButton tooltip and button behavior

--- components/shared/ConnectAwareButton/index.tsx
+++ components/shared/ConnectAwareButton/index.tsx
@@ -7,8 +7,13 @@ interface ConnectAwareButtonProps extends ButtonProps, PropsWithChildren {}
 export function ConnectAwareButton({ children, ...props }: ConnectAwareButtonProps) {
   const { isConnected } = useAccount()
   return (
-    <Tooltip label={!isConnected && 'You need to connect your wallet'}>
-      <Button isDisabled={!isConnected} {...props}>
+    <Tooltip label={!isConnected && 'You need to connect your wallet first'}>
+      <Button
+        {...props}
+        isDisabled={!isConnected}
+        _hover={!isConnected ? {} : undefined}
+        _active={!isConnected ? {} : undefined}
+      >
         {children}
       </Button>
     </Tooltip>
