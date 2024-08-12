be1abdb99ff0e9344fe92982f50dabca81d2ce35
Kyle <noll.kyle@gmail.com>
2024-08-12 10:31:22 -0800

    Update docs url

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,2 @@
-export const docsUrl =
-  'https://docs.ionprotocol.io/?_gl=1*4uhfq6*_ga*MzY3MjE0NzgzLjE3MTQ1Nzc3NjU.*_ga_6KGH2M44DE*MTcxODk1MjMyNi4xMi4wLjE3MTg5NTIzMjYuMC4wLjA.'
+export const docsUrl = 'https://docs.nucleusearn.io/'
 export const discordUrl = 'https://discord.com/invite/CjQqUgPA6Y'
