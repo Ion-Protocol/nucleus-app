93b63a4f7112df01b460e54088120aedb49106f0
Kyle <noll.kyle@gmail.com>
2024-08-28 14:11:21 -0800

    Update geoblock page

--- pages/geoblock.tsx
+++ pages/geoblock.tsx
@@ -1,12 +1,23 @@
-import GeoBlock from '@/components/shared/icons/GeoBlock'
+import { Image, useColorMode } from '@chakra-ui/react'
 import { Flex, Text } from '@chakra-ui/react'
 
 export default function Index({}) {
+  const colorMode = useColorMode()
   return (
-    <Flex direction="column" align="center" justify="center" mt={20}>
-      <GeoBlock />
-      <Flex h={6} />
-      <Text variant="large">Oops this app does not support users from your country. Sorry for the inconvenience!</Text>
+    <Flex justify="center" mt="150px">
+      <Flex direction="column" align="center" justify="center" mt={20} maxW="700px" gap={2}>
+        <Image
+          src={
+            colorMode.colorMode === 'dark' ? '/assets/images/geoblock-dark.png' : '/assets/images/geoblock-light.png'
+          }
+          alt="Geoblock"
+          width="350px"
+        />
+        <Flex h={6} />
+        <Text variant="heading2" lineHeight="1.5">
+          Oops this app does not support users from your country. Sorry for the inconvenience!
+        </Text>
+      </Flex>
     </Flex>
   )
 }
