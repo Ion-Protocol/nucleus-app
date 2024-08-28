f1cf7d54c856c7e9730157ccab48aa07f29d7ef7
Kyle <noll.kyle@gmail.com>
2024-08-28 14:11:40 -0800

    Add connect wallet button in place of mint when not connected

--- components/shared/ConnectAwareButton/index.tsx
+++ components/shared/ConnectAwareButton/index.tsx
@@ -1,18 +1,31 @@
 import { Button, ButtonProps } from '@chakra-ui/react'
+import { useConnectModal } from '@funkit/connect'
 import { PropsWithChildren } from 'react'
 import { useAccount } from 'wagmi'
-import { IonTooltip } from '../IonTooltip'
 
 interface ConnectAwareButtonProps extends ButtonProps, PropsWithChildren {}
 
 export function ConnectAwareButton({ children, ...props }: ConnectAwareButtonProps) {
+  const { isDisabled } = props
+  const { openConnectModal } = useConnectModal()
   const { isConnected } = useAccount()
-  const disabled = props.isDisabled || !isConnected
-  return (
-    <IonTooltip label={!isConnected && 'You need to connect your wallet first'}>
-      <Button {...props} _hover={disabled ? {} : undefined} _active={disabled ? {} : undefined} isDisabled={disabled}>
+
+  if (!isConnected) {
+    return (
+      <Button {...props} isDisabled={false} onClick={openConnectModal}>
+        Connect Wallet
+      </Button>
+    )
+  } else {
+    return (
+      <Button
+        {...props}
+        _hover={isDisabled ? {} : undefined}
+        _active={isDisabled ? {} : undefined}
+        isDisabled={isDisabled}
+      >
         {children}
       </Button>
-    </IonTooltip>
-  )
+    )
+  }
 }
