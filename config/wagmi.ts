3ea24fd2a8276891f4a11e3dd1c676a0190ae511
Kyle <noll.kyle@gmail.com>
2024-09-11 00:06:53 -0800

    Add rpc urls for wagmi config

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,8 +1,11 @@
 import { createFunkitWagmiConfig, getDefaultTransports, getDefaultWallets } from '@funkit/connect'
 import { bitgetWallet, ledgerWallet } from '@funkit/connect/wallets'
-import { http } from 'wagmi'
+import { fallback, http } from 'wagmi'
 import { mainnet } from 'wagmi/chains'
 import { sei, tenderlyStaging } from './tenderly'
+
+const MAINNET_CHAINSTACK_URL = process.env.NEXT_PUBLIC_MAINNET_CHAINSTACK_URL || ''
+const SEI_RPC_URL = process.env.NEXT_PUBLIC_SEI_RPC_URL || ''
 
 const WALLET_CONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || ''
 
@@ -29,8 +32,9 @@ export const wagmiConfig = createFunkitWagmiConfig({
   chains,
   transports: {
     ...getDefaultTransports(),
+    [mainnet.id]: fallback([http(MAINNET_CHAINSTACK_URL)]),
     [tenderlyStaging.id]: http(),
-    [sei.id]: http(),
+    [sei.id]: fallback([http(SEI_RPC_URL)]),
   },
   ssr: true,
 })
