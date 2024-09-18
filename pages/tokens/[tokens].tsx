239d5ac4f0e2c75bcb8ec314216c33028c09f378
Kyle <noll.kyle@gmail.com>
2024-09-17 22:49:05 -0800

    Remove unnecessary null check

--- pages/tokens/[tokens].tsx
+++ pages/tokens/[tokens].tsx
@@ -3,19 +3,9 @@ import NetworkAssetTitle from '@/components/NetworkAsset/NetworkAssetTitle'
 import Apy from '@/components/NetworkAsset/NetworkAssetTitle/Apy'
 import RewardsAndPoints from '@/components/NetworkAsset/NetworkAssetTitle/RewardsAndPoints'
 import Tvl from '@/components/NetworkAsset/NetworkAssetTitle/Tvl'
-import { useAppSelector } from '@/store/hooks'
-import { selectNetworkAssetFromRoute } from '@/store/slices/router'
 import { Flex } from '@chakra-ui/react'
 
 export default function Token() {
-  // Make sure the router query properly contains the network asset key before loading the network asset page.
-  // This should only take one-ish iteration to complete, so there is no visible loading state.
-  // Now we can assert that chainKey is not null for every child component.
-  const chainKey = useAppSelector(selectNetworkAssetFromRoute)
-  if (chainKey === null) {
-    return <></>
-  }
-
   return (
     <Flex direction="column" w="100%" align="center">
       <Flex direction="column" h="100%" pt={20}>
