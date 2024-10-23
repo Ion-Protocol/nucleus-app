f71b0667b63d8dcf98a57006ddb7610be908ea6f
Rowdy <mark@molecularlabs.io>
2024-10-23 16:07:48 -0600

    fix(mint): added exchange rate and destination token ticker. exchange rate is displayed truncated on the ui with a full exchange rate displayed on hover

--- components/NetworkAsset/MintAndRedeem/shared/Summary/connector.ts
+++ components/NetworkAsset/MintAndRedeem/shared/Summary/connector.ts
@@ -1,23 +1,41 @@
+import { ConnectedProps, connect } from 'react-redux'
+
 import { RootState } from '@/store'
+import { tokensConfig } from '@/config/tokens'
+
+import { bigIntToNumberAsString } from '@/utils/bigint'
 import {
   selectFormattedPreviewFee,
-  selectFormattedTokenRateInQuote,
+  selectTokenRateInQuote,
+  selectTokenRateInQuoteLoading,
   selectNetworkAssetConfig,
   selectPreviewFeeLoading,
   selectSourceChainKey,
 } from '@/store/slices/networkAssets'
-import { ConnectedProps, connect } from 'react-redux'
+import { selectNetworkAssetFromRoute } from '@/store/slices/router'
 
 const mapState = (state: RootState, ownProps: SummaryOwnProps) => {
   let selectedChainKey = selectSourceChainKey(state)
   const networkAssetConfig = selectNetworkAssetConfig(state)
+  const tokenRateInQuote = selectTokenRateInQuote(state)
+  const networkAssetFromRoute = selectNetworkAssetFromRoute(state)
+  const networkAssetName = networkAssetFromRoute ? tokensConfig[networkAssetFromRoute].name : ''
+  const exchangeRate = tokenRateInQuote
+    ? bigIntToNumberAsString(BigInt(tokenRateInQuote), { maximumFractionDigits: 18 })
+    : '0.00'
+  const truncatedExchangeRate = tokenRateInQuote
+    ? bigIntToNumberAsString(BigInt(tokenRateInQuote), { maximumFractionDigits: 4 })
+    : '0.00'
   const receiveOn = networkAssetConfig?.receiveOn
   const isSameChain = selectedChainKey === receiveOn
 
   return {
     fees: selectFormattedPreviewFee(state),
     loading: selectPreviewFeeLoading(state),
-    exchangeRate: selectFormattedTokenRateInQuote(state),
+    exchangeRate,
+    truncatedExchangeRate,
+    exchangeRateLoading: selectTokenRateInQuoteLoading(state),
+    networkAssetName,
     isSameChain,
   }
 }
--- components/NetworkAsset/MintAndRedeem/shared/Summary/index.tsx
+++ components/NetworkAsset/MintAndRedeem/shared/Summary/index.tsx
@@ -4,10 +4,19 @@ import { InfoOutlineIcon } from '@chakra-ui/icons'
 import { Flex, Text } from '@chakra-ui/react'
 import { SummaryConnector } from './connector'
 
-function Summary({ fees, loading, isSameChain }: SummaryConnector.Props) {
+function Summary({
+  fees,
+  loading,
+  isSameChain,
+  exchangeRate,
+  truncatedExchangeRate,
+  exchangeRateLoading,
+  networkAssetName,
+}: SummaryConnector.Props) {
   const tooltipLabel = isSameChain
     ? 'No fees are charged when depositing and minting on the same chain'
     : 'Fees are charged by the underlying bridge provider such as LayerZero or Hyperlane'
+  const exchangeRateTooltipLabel = 'The exchange rate is the ratio of the deposit amount to the minted amount.'
   return (
     <Flex direction="column" gap={3}>
       <Flex align="center" justify="space-between">
@@ -15,14 +24,16 @@ function Summary({ fees, loading, isSameChain }: SummaryConnector.Props) {
           <Text variant="paragraph" color="disabledText">
             Exchange Rate
           </Text>
-          <IonTooltip label={tooltipLabel}>
+          <IonTooltip label={exchangeRateTooltipLabel}>
             <InfoOutlineIcon color="infoIcon" mt={'2px'} fontSize="sm" />
           </IonTooltip>
         </Flex>
-        <IonSkeleton minW="75px" isLoaded={!loading}>
-          <Text textAlign="right" variant="paragraph">
-            {fees}
-          </Text>
+        <IonSkeleton minW="35px" isLoaded={!exchangeRateLoading}>
+          <IonTooltip label={exchangeRate}>
+            <Text textAlign="right" variant="paragraph">
+              {`WETH / ${networkAssetName} ${truncatedExchangeRate}`}
+            </Text>
+          </IonTooltip>
         </IonSkeleton>
       </Flex>
       <Flex align="center" justify="space-between">
