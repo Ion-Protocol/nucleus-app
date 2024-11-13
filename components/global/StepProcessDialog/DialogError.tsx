b6dddb19c44e3aeee8b4df4b35bcac4e47561e02
Rowdy <mark@molecularlabs.io>
2024-11-13 07:22:00 +0700

    fix(dialog-error-state): updated to hide placeholder for dialog error state

--- components/global/StepProcessDialog/DialogError.tsx
+++ components/global/StepProcessDialog/DialogError.tsx
@@ -7,14 +7,15 @@ const DialogError = () => {
     <Flex flexDirection={'column'} gap={2} width={'full'} justifyContent={'center'} alignItems={'center'}>
       <FullErrorIcon />
       <Heading fontSize={'bigParagraph'} textAlign={'center'}>
-        Ops! Something went wrong
+        Oops! Something went wrong
       </Heading>
-      <Text textAlign={'left'}>Error: User rejected the request</Text>
-      <Code>
+      {/* TODO: Better error messages from contract */}
+      {/* <Text textAlign={'left'}>Error: Ops looks like something went wrong</Text> */}
+      {/* <Code>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores recusandae eum quidem, voluptas reiciendis
         tenetur aut labore quos? Porro ratione eum velit ea recusandae sit laboriosam beatae unde veritatis
         necessitatibus.
-      </Code>
+      </Code> */}
     </Flex>
   )
 }
