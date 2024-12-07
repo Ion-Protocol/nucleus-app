d4933e0239eefba2aca604e163adbdb7216dcdfd
Rowdy <mark@molecularlabs.io>
2024-12-06 20:12:07 -0700

    feat(nucleusBackendApi.ts,-package.json): wrote query for withdrawals. updated viem and wagmi
    BREAKING CHANGE:

--- store/slices/nucleusBackendApi.ts
+++ store/slices/nucleusBackendApi.ts
@@ -8,26 +8,64 @@ interface RewardsAPYParams {
   lookBackDays?: number // API defaults to 7 days if not provided
 }
 
-interface BulkRewardsAPYParams {
-  params: RewardsAPYParams[]
-}
-
-interface BulkRewardsAPYResponse {
-  results: {
-    tokenAddress: Address
-    response: RewardsAPYResponse | null
-    error?: string
-  }[]
-}
-
 interface RewardsAPYResponse {
   apy: number
 }
+type WithdrawalStatus = 'all' | 'pending' | 'completed' | 'cancelled'
+export type WithdrawalParams = {
+  user: Address
+  vaultAddress: Address
+  chainId: number
+  status?: WithdrawalStatus
+  all?: boolean
+  page?: number
+  limit?: number
+}
 
+export interface Withdrawal {
+  id: number
+  user: string
+  offer_token: string
+  want_token: string
+  amount: string
+  deadline: string
+  atomic_price: string
+  created_timestamp: string
+  ending_timestamp: string
+  created_log_index: number
+  created_transaction_index: number
+  created_block_number: string
+  ending_log_index: number
+  ending_transaction_index: number
+  ending_block_number: string
+  status: WithdrawalStatus
+  queue_address: string
+  chain_id: number
+  offer_amount_spent: string
+  want_amount_rec: string
+  created_transaction_hash: string
+  ending_transaction_hash: string
+}
+
+export interface Pagination {
+  currentPage: number
+  pageSize: number
+  totalItems: number
+  totalPages: number
+  hasNextPage: boolean
+  hasPreviousPage: boolean
+}
+export interface WithdrawalsResponse {
+  data: Withdrawal[]
+  pagination: Pagination
+}
 // Define a service using a base URL and expected endpoints
 export const nucleusBackendApi = createApi({
   reducerPath: 'nucleusBackendApi',
-  baseQuery: fetchBaseQuery({ baseUrl: nucleusBackendBaseUrl }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: nucleusBackendBaseUrl,
+    mode: 'cors',
+  }),
   endpoints: (builder) => ({
     // Keep the original single query endpoint
     getDefaultYieldAPY: builder.query<RewardsAPYResponse, RewardsAPYParams>({
@@ -39,7 +77,20 @@ export const nucleusBackendApi = createApi({
         return `v1/vaults/apy?${params.toString()}`
       },
     }),
+    withdrawalsByUser: builder.query<WithdrawalsResponse, WithdrawalParams>({
+      query: ({ user, vaultAddress, chainId, status = 'all', all, page, limit }) => {
+        const params = new URLSearchParams()
+        params.append('user', user)
+        params.append('vault_address', vaultAddress)
+        params.append('chain_id', chainId.toString())
+        if (status) params.append('status', status)
+        if (all) params.append('all', all.toString())
+        if (page) params.append('page', page.toString())
+        if (limit) params.append('limit', limit.toString())
+        return `v1/withdrawals?${params.toString()}`
+      },
+    }),
   }),
 })
 
-export const { useGetDefaultYieldAPYQuery } = nucleusBackendApi
+export const { useGetDefaultYieldAPYQuery, useWithdrawalsByUserQuery } = nucleusBackendApi
