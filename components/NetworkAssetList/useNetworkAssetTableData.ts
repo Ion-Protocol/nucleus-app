818f5118f45d4c5f746e276af5cd2ed67b0de5cf
Kyle <noll.kyle@gmail.com>
2025-01-26 07:01:57 -0900

    Apply sorting to dashboard cards

--- components/NetworkAssetList/useNetworkAssetTableData.ts
+++ components/NetworkAssetList/useNetworkAssetTableData.ts
@@ -13,6 +13,7 @@ export function useNetworkAssetTableData() {
   const dispatch = useAppDispatch()
 
   const [data, setData] = useState<DashboardTableDataItem[]>([])
+  const [isLoading, setIsLoading] = useState(false)
 
   const usdPerEthRate = useAppSelector(selectUsdPerEthRate)
   const bridgesState = useAppSelector(selectBridgesState)
@@ -21,42 +22,58 @@ export function useNetworkAssetTableData() {
   const networkAssetKeys = useMemo(() => Object.keys(networkAssetConfig) as TokenKey[], [networkAssetConfig])
 
   const fetchRewardsAPY = useCallback(async () => {
-    const data: DashboardTableDataItem[] = []
-    for (const networkAssetKey of networkAssetKeys) {
-      const vaultAddress = networkAssetConfig[networkAssetKey]?.contracts.boringVault
-      if (!vaultAddress) return { networkAssetKey, apy: 0 }
+    try {
+      setIsLoading(true)
+      const promises = networkAssetKeys.map(async (networkAssetKey) => {
+        const vaultAddress = networkAssetConfig[networkAssetKey]?.contracts.boringVault
+        if (!vaultAddress) {
+          return {
+            asset: networkAssetKey,
+            apy: '0%',
+            chain: networkAssetConfig[networkAssetKey]?.chain,
+            tvl: { formatted: '0', value: 0 },
+            applications: networkAssetConfig[networkAssetKey]?.protocols || [],
+          }
+        }
 
-      // Make the API call to get the APY
-      const result = await dispatch(
-        nucleusBackendApi.endpoints.getDefaultYieldAPY.initiate({ tokenAddress: vaultAddress })
-      )
-      const apy = result?.data?.apy ?? 0
-      const formattedApy = numberToPercent(apy, 2)
+        // Make the API call to get the APY
+        const result = await dispatch(
+          nucleusBackendApi.endpoints.getDefaultYieldAPY.initiate({ tokenAddress: vaultAddress })
+        )
 
-      const tvlAsBigIntString = bridgesState.tvl.data[networkAssetKey]
-      const tvlAsBigInt = BigInt(tvlAsBigIntString)
-      const tvlInUsdAsBigInt = (tvlAsBigInt * usdPerEthRate) / BigInt(1e8)
-      const tvlInUsdAsNumber = bigIntToNumber(tvlInUsdAsBigInt)
-      const formattedTvl = abbreviateNumber(tvlInUsdAsNumber)
+        const apy = result?.data?.apy ?? 0
+        const formattedApy = numberToPercent(apy, 2)
 
-      data.push({
-        asset: networkAssetKey,
-        apy: formattedApy,
-        chain: networkAssetConfig[networkAssetKey]?.chain,
-        tvl: {
-          formatted: formattedTvl,
-          value: tvlInUsdAsNumber,
-        },
-        applications: networkAssetConfig[networkAssetKey]?.protocols || [],
+        const tvlAsBigIntString = bridgesState.tvl.data[networkAssetKey]
+        const tvlAsBigInt = BigInt(tvlAsBigIntString)
+        const tvlInUsdAsBigInt = (tvlAsBigInt * usdPerEthRate) / BigInt(1e8)
+        const tvlInUsdAsNumber = bigIntToNumber(tvlInUsdAsBigInt)
+        const formattedTvl = abbreviateNumber(tvlInUsdAsNumber)
+
+        return {
+          asset: networkAssetKey,
+          apy: formattedApy,
+          chain: networkAssetConfig[networkAssetKey]?.chain,
+          tvl: {
+            formatted: formattedTvl,
+            value: tvlInUsdAsNumber,
+          },
+          applications: networkAssetConfig[networkAssetKey]?.protocols || [],
+        }
       })
-    }
 
-    setData(data)
+      const resolvedData = await Promise.all(promises)
+      setData(resolvedData)
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoading(false)
+    }
   }, [networkAssetKeys, networkAssetConfig, bridgesState, usdPerEthRate, dispatch])
 
   useEffect(() => {
     fetchRewardsAPY()
   }, [fetchRewardsAPY])
 
-  return data
+  return { data, isLoading }
 }
