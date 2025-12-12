5b41006c727959d341e2dd7278e19d86bc6049d9
Rowdy <mark@molecularlabs.io>
2025-12-12 13:21:36 -0700

    feat: make deprecation modal permanent with withdrawal docs link
    The app is now fully deprecated. Users cannot dismiss the modal
    and are directed to the documentation for manual withdrawal instructions.

--- hooks/useDeprecationModal.ts
+++ hooks/useDeprecationModal.ts
@@ -1,4 +1,3 @@
-import { DEPRECATION_CONFIG } from '@/config/deprecation'
 import { openDeprecationModal } from '@/store/slices/ui/slice'
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
@@ -7,17 +6,11 @@ export function useDeprecationModal() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    // Check if user has dismissed the modal before
-    const hasUserDismissed = localStorage.getItem(DEPRECATION_CONFIG.MODAL_DISMISSED_KEY) === 'true'
+    // Always show the deprecation modal - app is fully deprecated
+    const timer = setTimeout(() => {
+      dispatch(openDeprecationModal())
+    }, 500)
 
-    // Show modal on every visit until dismissed (as per requirement)
-    if (!hasUserDismissed) {
-      // Small delay to ensure app is fully loaded
-      const timer = setTimeout(() => {
-        dispatch(openDeprecationModal())
-      }, 1000)
-
-      return () => clearTimeout(timer)
-    }
+    return () => clearTimeout(timer)
   }, [dispatch])
 }
